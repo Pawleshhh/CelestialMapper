c459f46cf2e651fdef8c11f78d35b241834af044
Mateusz Pawlikowski <30951232+Pawleshhh@users.noreply.github.com>
2023-03-15 19:31:58 +0100

    Add ResourceHelper

---
+++ Source/CelestialMapper.Common/ResourceHelper.cs
@@ -0,0 +1,12 @@
+﻿namespace CelestialMapper.Common;
+
+public static class ResourceHelper
+{
+
+    public static Stream GetResource<T>(string path)
+    {
+        var resource = typeof(T).Assembly.GetManifestResourceStream(path);
+        return resource ?? Stream.Null;
+    }
+
+}
---
+++ Source/CelestialMapper.Core/Database/DatabaseManager.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Data.Sqlite;
+using System.Data.Common;
+
+namespace CelestialMapper.Core;
+
+public class DatabaseManager
+{
+
+    private static readonly string databasePath = "./stars.sqlite";
+
+    public IEnumerable<CelestialObject> GetCelestialObjects(Func<CelestialObject, bool>? predicate = null)
+    {
+        predicate ??= _ => true;
+
+        using DbConnection connection = new SqliteConnection($"Data Source={databasePath}");
+        connection.Open();
+
+        using DbCommand command = connection.CreateCommand();
+
+        throw new NotImplementedException();
+    }
+
+}
