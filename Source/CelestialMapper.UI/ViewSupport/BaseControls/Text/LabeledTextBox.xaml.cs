da273beaee57bfc688fc83e19e674553b586892f
Mateusz Pawlikowski (MTWP) <mtwp@demant.com>
2024-03-29 13:23:40 +0100

    LabeledText - part 1 - style and converter for text placement

---
+++ Source/CelestialMapper.UI/ViewSupport/BaseControls/Text/LabeledTextBox.xaml.cs
@@ -0,0 +1,32 @@
+﻿namespace CelestialMapper.UI;
+
+/// <summary>
+/// Interaction logic for LabeledTextBox.xaml
+/// </summary>
+public partial class LabeledTextBox : TextBox
+{
+
+    public LabeledTextBox()
+    {
+        InitializeComponent();
+    }
+
+    public string Label
+    {
+        get { return (string)GetValue(LabelProperty); }
+        set { SetValue(LabelProperty, value); }
+    }
+
+    public static readonly DependencyProperty LabelProperty =
+        DependencyProperty.Register(nameof(Label), typeof(string), typeof(LabeledTextBox), new PropertyMetadata(string.Empty));
+
+    public TextPlacement LabelPlacement
+    {
+        get { return (TextPlacement)GetValue(LabelPlacementProperty); }
+        set { SetValue(LabelPlacementProperty, value); }
+    }
+
+    public static readonly DependencyProperty LabelPlacementProperty =
+        DependencyProperty.Register(nameof(LabelPlacement), typeof(TextPlacement), typeof(LabeledTextBox), new PropertyMetadata(TextPlacement.Left));
+
+}
---
+++ Source/CelestialMapper.UI/ViewSupport/Converters/TextPlacementToDockConverter.cs
@@ -0,0 +1,50 @@
+﻿using System.Globalization;
+
+namespace CelestialMapper.UI;
+
+public class TextPlacementToDockConverter : ValueConverterBase<TextPlacement, Dock, object>
+{
+    public static TextPlacementToDockConverter Instance { get; } = new TextPlacementToDockConverter();
+
+    public override bool ExpectsParameter => false;
+
+    public override OnWrongType OnWrongType => OnWrongType.ReturnDefault;
+
+    public override TextPlacement DefaultFromValue => TextPlacement.Left;
+
+    public override Dock DefaultToValue => Dock.Left;
+
+    public override Dock Convert(TextPlacement value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return TextPlacementToDock(value);
+    }
+
+    public override TextPlacement ConvertBack(Dock value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return DockToTextPlacement(value);
+    }
+
+    public static Dock TextPlacementToDock(TextPlacement textPlacement)
+    {
+        return textPlacement switch
+        {
+            TextPlacement.Left => Dock.Left,
+            TextPlacement.Right => Dock.Right,
+            TextPlacement.Top => Dock.Top,
+            TextPlacement.Bottom => Dock.Bottom,
+            _ => Dock.Left
+        };
+    }
+
+    public static TextPlacement DockToTextPlacement(Dock dock)
+    {
+        return dock switch
+        {
+            Dock.Left => TextPlacement.Left,
+            Dock.Right => TextPlacement.Right,
+            Dock.Top => TextPlacement.Top,
+            Dock.Bottom => TextPlacement.Bottom,
+            _ => TextPlacement.Left
+        };
+    }
+}
