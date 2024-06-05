26b34e1785cafcbdb72f4cffe36352ac3c1edaac
Mateusz Pawlikowski <mateuszpawlikowski98@gmail.com>
2024-06-05 20:12:47 +0200

    Implement VisualToPdf

---
+++ Source/CelestialMapper.UI/Managers/VisualToPdf/IVisualToPdf.cs
@@ -0,0 +1,11 @@
+﻿using System.Windows.Media;
+using System.Windows.Xps.Packaging;
+
+namespace CelestialMapper.UI;
+
+public interface IVisualToPdf
+{
+
+    public void XpsToPdf(Visual visual, string pdfPath);
+
+}
---
+++ Source/CelestialMapper.UI/Managers/VisualToPdf/VisualToPdf.cs
@@ -0,0 +1,26 @@
+﻿using System.IO;
+using System.Windows.Media;
+using System.Windows.Xps;
+using System.Windows.Xps.Packaging;
+
+namespace CelestialMapper.UI;
+
+public class VisualToPdf : IVisualToPdf
+{
+    public void XpsToPdf(Visual visual, string pdfPath)
+    {
+        using (MemoryStream memoryStream = new MemoryStream())
+        {
+            System.IO.Packaging.Package package = System.IO.Packaging.Package.Open(memoryStream, FileMode.Create);
+            XpsDocument xpsDocument = new XpsDocument(package);
+            XpsDocumentWriter xpsDocumentWriter = XpsDocument.CreateXpsDocumentWriter(xpsDocument);
+
+            xpsDocumentWriter.Write(visual);
+            xpsDocument.Close();
+            package.Close();
+
+            var pdfXpsDoc = PdfSharp.Xps.XpsModel.XpsDocument.Open(memoryStream);
+            PdfSharp.Xps.XpsConverter.Convert(pdfXpsDoc, pdfPath, 0);
+        }
+    }
+}
