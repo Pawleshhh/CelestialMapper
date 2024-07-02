42da903c4af246b3576be2fb0aeabb775c9157fc
Mateusz Pawlikowski <mateuszpawlikowski98@gmail.com>
2024-07-02 19:19:34 +0200

    MenuView unit tests

---
+++ Source/CelestialMapper.UI.Test/Features/Menu/MenuViewTest.cs
@@ -0,0 +1,14 @@
+﻿using CelestialMapper.ViewModel;
+using Moq;
+
+namespace CelestialMapper.UI.Test;
+
+[TestFixture]
+public class MenuViewTest : FeatureViewTest<MenuView, MenuViewModel>
+{
+    public override FeatureName DefaultFeatureName => new("Menu");
+
+    public override Func<IServiceProvider, MenuView> FeatureViewFactory
+        => s => new MenuView(ServiceProvider.Object, false);
+
+}
