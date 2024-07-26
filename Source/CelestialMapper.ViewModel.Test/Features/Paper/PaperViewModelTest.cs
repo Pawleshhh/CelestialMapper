b9995e6d7f7ec0c2a8d60bafb5036946998685f2
Mateusz Pawlikowski <mateuszpawlikowski98@gmail.com>
2024-07-26 12:44:18 +0200

    PaperViewModel unit tests

---
+++ Source/CelestialMapper.ViewModel.Test/Features/Paper/PaperViewModelTest.cs
@@ -0,0 +1,10 @@
+﻿namespace CelestialMapper.ViewModel.Test;
+
+[TestFixture]
+public class PaperViewModelTest : ViewModelTest<PaperViewModel>
+{
+    public override Func<PaperViewModel> CreateSUT => () => new PaperViewModel(ViewModelSupport.Object);
+
+    public override FeatureName DefaultFeatureName => new("Paper");
+
+}
